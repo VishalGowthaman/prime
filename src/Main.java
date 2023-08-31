78e417ecd5e80e332c42251d1a72f08d5be5d220
VishalGowthaman <vishalranga7619@gmail.com>
2023-08-31 11:39:23 +0530

    prime

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,15 @@ public class Main {
         }
     }
 
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <num/2; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
-    }git add
+    }
 }
